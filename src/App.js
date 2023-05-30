1f854766344b9c4c40f7d094ee0420002936eca1
Kris Thornton <Kristhornton@ymail.com>
2023-05-29 17:54:56 -0700

    setting up main markup

--- src/App.js
+++ src/App.js
@@ -1,49 +1,11 @@
-import logo from './logo.svg';
-import React, { useState } from 'react';
-import Form from './components/Form';
-import './App.css';
-
-function App() {
-
-  const [members, setMembers] = useState([]);
-  const [values, setValues] = useState('');
-
-
-  const onSubmit = (evt) => {
-    console.log(setMembers)
-    setMembers([values, ...members])
-    
-  }
-
-  const onChange = (name, value) => {
-    setValues({ ...values, [name]: value })
-    
-  }
-
+import React, { useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
+export default function App() {
   return (
-    <div>
-      <h1>Name Lister</h1>
-      <h3>Please Enter your first and last name below.</h3>
-      <Form
-        values={values}
-        change={onChange}
-        submit={onSubmit}
-
-      />
-
-      {members.map((member, idx) => {
-        return (
-          <div key={idx}>
-            <li>
-              {member.name}
-          
-            </li>
-          </div>
-        )
-      })}
+    <div className='App'>
+      <h1>Team Builder App</h1>
+      <Form />
     </div>
   );
-
 }
-
-export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,51 +1,45 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-export default function Form  (props)  {
+// Change Handlers
 
-    // const [peopleList, setPeopleList] = useState({fName:'', lName:''});
-
-
-    const changeList = evt => {
-        const { name, value } = evt.target;
-
-        props.change(name, value);
-      }
-
-
-    const handleSubmit = evt => {
-        evt.preventDefault();
-        props.submit();
-        
     
-      }
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    First Name:
-                    <input
-                        type='text'
-                        name='First Name'
-                        value={props.values.name}
-                        onChange={changeList}
-                    />
+
+
+// --------------------Markup that is passed to App.js for DOM----------
+
+    const Form = (props) => {
+        return (
+            <form>
+                <label>Name
+                <input
+                    placeholder='name'
+                    value={values.name}
+                    name='name'
+                    onChange={handleChange}
+                />
                 </label>
-                <label>
-                    Last Name:
-                    <input
-                        type='text'
-                        name='Last Name'
-                        value={props.values.name}
-                        onChange={changeList}
-                    />
+                <label>Email
+                <input
+                    placeholder='Enter Email'
+                    value={values.email}
+                    name='email'
+                    onChange={handleChange}
+                />
                 </label>
-                <input type='submit' value='Add to List!' />
+                <label>Role
+                <input  
+                    placeholder='Enter your role here'
+                    value={values.role}
+                    name='role'
+                    onChange={handleChange}
+                />
+                </label>
+                <input type='submit' value="Create your team" />
             </form>
-            <ul>
-                
-            </ul>
-        </div>
+        )
+    }
 
-    )
-}
+
+export default Form;
+    
